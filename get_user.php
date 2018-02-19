5b00983da6c4382d542f6840631630ecc5be5c6f
Jesus Marquez <jamarquez@uao.edu.co>
2018-02-19 13:26:54 -0500

    Muestra data de usuario encontrado

--- get_user.php
+++ get_user.php
@@ -6,7 +6,7 @@ if ($token) {
 	$curl=curl_init($service_url);
 	$curl_post_data = array('token'=>$token,'method'=>'get_user', 'username'=>'guest');
 	curl_setopt($curl, CURLOPT_POST, true);
-	curl_setopt($curl, CURLOPT_POSTFIELDS, array('token'=>$token,'method'=>'get_user', 'value'=>'david.maquez', 'field'=>'username'));
+	curl_setopt($curl, CURLOPT_POSTFIELDS, array('token'=>$token,'method'=>'get_user', 'value'=>'david.marquez', 'field'=>'username'));
 	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
 	$curl_response = curl_exec($curl);
 	if ($curl_response === false) {
@@ -22,21 +22,17 @@ if ($token) {
 	
 	$status = $xml_body->get_user->status;
 	if ((string)$status == 'success') {
+		$user_xml = $xml_body->get_user->user;
+		$json = json_encode($user_xml);
+		$user_array = json_decode($json,TRUE);
+		$data = array("username" => $user_array['username'], "email" => $user_array['email']);
+		var_dump($data);
 		printf ("SUCCESS!\n");
 	} 
 	
 	if ((string)$status == 'failed') {
 		printf ("failed!\n");
 	}
-	
-	
-	/*
-	$user_xml = $xml_body->get_user->user;
-	$json = json_encode($user_xml);
-	$user_array = json_decode($json,TRUE);
-	$data = array("username" => $user_array['username'], "email" => $user_array['email']);
-	var_dump($data);
-	*/
 
 } else {
 	printf("ERR:token no existe!\n");
