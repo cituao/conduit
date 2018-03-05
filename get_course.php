92bcc3a9c9dd53498a1c5864dfbdca50a884fa86
Jesus Marquez <jamarquez@uao.edu.co>
2018-03-05 15:27:57 -0500

    getcourse

---
+++ get_course.php
@@ -0,0 +1,54 @@
+<?php
+/* *********************************************************
+script para obtener curso de moodlerooms
+
+
+************************************************************/
+
+// datos del curso a buscar
+$curso = array(
+    "value" => "prueba-onduit",
+	"field" => "shortname");
+
+$token = FALSE;
+$token =  getenv('TOKEN_MR');
+if ($token) {
+	$service_url = 'https://uao-sandbox.mrooms.net/blocks/conduit/webservices/rest/course.php';
+	$curl=curl_init($service_url);
+	$curl_post_data = array('token'=>$token,'method'=>'get_course', 'username'=>'guest');
+	curl_setopt($curl, CURLOPT_POST, true);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, array('token'=>$token,'method'=>'get_course', 'value'=>$curso['value'], 'field'=>$curso['field']));
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+	$curl_response = curl_exec($curl);
+	if ($curl_response === false) {
+		$info = curl_getinfo($curl);
+		curl_close($curl);
+		die('error occured during curl exec. Additioanl info: ' . var_export($info));
+	}
+	curl_close($curl);
+	printf("*****XML RESPONSE CONDUIT*****\n");
+	print_r($curl_response);
+	
+	// convertir en un objeto simpleXML
+	$xml_body = simplexml_load_string($curl_response);
+	
+	
+	$status = $xml_body->get_course->status;
+	if ((string)$status == 'success') {
+		$course_xml = $xml_body->get_course->course;
+		$json = json_encode($course_xml);
+		$course_array = json_decode($json,TRUE);
+		$data = array("shortname" => $course_array['shortname'], "idnumber" => $course_array['idnumber'], "status" => "success");
+	}
+
+	if ((string)$status == 'failed') {
+		$message = (string)$xml_body->get_course->response->message;
+		$data = array("status" => "failed", "message" => $message);
+	}
+	
+	
+} else {
+	$data = array("status" => "failed", "message" => "Token no existe");
+}
+var_dump($data);
+?>
