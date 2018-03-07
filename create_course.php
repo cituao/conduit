09ad3dfc524a2b81e33bcc4d6acb2d09ec123d70
Jesus Marquez <jamarquez@uao.edu.co>
2018-03-07 14:54:51 -0500

    createcourse

--- create_course.php
+++ create_course.php
@@ -5,39 +5,32 @@
    
    Url: https://uao-sandbox.mrooms.net/
 ***************************************** */
+include 'scheme_conduit_course.php';
 
-include 'scheme_conduit_user.php';
-
-$service_url = 'https://uao-sandbox.mrooms.net/blocks/conduit/webservices/rest/user.php';
+$service_url = 'https://uao-sandbox.mrooms.net/blocks/conduit/webservices/rest/course.php';
 
 // personalice el array con el usuario a crear
-$data = array(
-    "username" => "invitado5",
-	"password" => "Uao.2018",
-    "nombre" => "invitado11",
-    "apellido" => "invitado11",
-    "email" => "invitado11@uao.edu.co",
-    "auth" => "manual");
+$data_course = array(
+    "shortname" => "curso_prueba_600",
+    "fullname" => "Curso prueba 600",
+	"category" => "7");
 // crea un objeto simplexml y carga schema xml tipo user
-$xml_user = new SimpleXMLElement($xmlstr);
+$xml_course = new SimpleXMLElement($xmlstr);
 
 // cambiamos los valores elementos con los datos de usuario a crear
 //$xml_user->datum->mapping[0][0] = $data["username"];
-$xml_user->datum->mapping[0][0] = $data["username"];
-$xml_user->datum->mapping[1][0] = $data["password"];
-$xml_user->datum->mapping[2][0] = $data["nombre"];
-$xml_user->datum->mapping[3][0] = $data["apellido"];
-$xml_user->datum->mapping[4][0] = $data["email"];
-$xml_user->datum->mapping[5][0] = $data["auth"];
+$xml_course->datum->mapping[0][0] = $data_course["shortname"];
+$xml_course->datum->mapping[1][0] = $data_course["fullname"];
+$xml_course->datum->mapping[2][0] = $data_course["category"];
 
-
-$xml_user_str = $xml_user->asXML();
+// convierte objeto simpleXML en strign
+$xml_course_str = $xml_course->asXML();
 
 $token = FALSE;
 $token =  getenv('TOKEN_MR');
 if ($token) {
     $curl=curl_init($service_url);
-    $curl_post_data = array('token'=>$token,'method'=>'handle', 'xml'=>$xml_user_str);
+    $curl_post_data = array('token'=>$token,'method'=>'handle', 'xml'=>$xml_course_str);
     curl_setopt($curl, CURLOPT_POST, true);
     curl_setopt($curl, CURLOPT_POSTFIELDS, $curl_post_data);
     curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
@@ -52,7 +45,8 @@ if ($token) {
 	printf("*****XML RESPONSE CONDUIT*****\n");
     print_r($curl_response);
 	// la respuesta es un string xml
-	// lo convertimos el xml en un objeto de simpleXML
+	// convertimos el xml en un objeto de simpleXML
+	
 	$xml_response = simplexml_load_string($curl_response);
 	
 	//obtenemos el status 
@@ -65,6 +59,7 @@ if ($token) {
 		$message = (string)$xml_response->handle->message;
 		$data = array("status" => "failed", "message" => $message);
 	}
+
 	
 } 
 else {
---
+++ scheme_conduit_course.php
@@ -0,0 +1,12 @@
+<?php
+$xmlstr = <<<XML
+<?xml version="1.0" encoding="UTF-8"?>
+<data>
+<datum action="create">
+<mapping name="shortname"></mapping>
+<mapping name="fullname"></mapping>
+<mapping name="category"></mapping>
+</datum>
+</data>
+XML;
+?>
